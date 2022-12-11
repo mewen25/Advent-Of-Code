bd2d73ae3f82bb80fc34bf79dc1fb64b3b6a26c2
Ewan <mewen25@gmail.com>
2022-12-11 16:31:09 +0000

    day 11

--- days/day11/part1.ts
+++ days/day11/part1.ts
@@ -1,3 +1,10 @@
 export default function part1(data: string) {
-  return 0;
+  const monkeys = data.split('\n\n').map(m => {
+    const id = m.match(/\d+:/)[0].slice(0, -1);
+    const items = m.match(/(items:\s)(.+\n)/)[0]
+    const oldOp = m.match(/(new = old\s)(.+\n)/)[1];
+    const test = m.match(/(by\s)(.+\n)/)[1];
+    console.log(id, items, oldOp, test)
+  });
+  // console.log(monkeys)
 }
